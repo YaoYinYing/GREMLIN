13a3b748c79bbe8cc6eb4b65e31d9055e17cda88
yaoyy.hi@gmail.com <33014714+YaoYinYing@users.noreply.github.com>
2022-04-26 11:50:58 +0800

    Update run_dimer_pipline.sh

--- run_dimer_pipline.sh
+++ run_dimer_pipline.sh
@@ -30,7 +30,7 @@ while getopts ":i:f:d:a:r:m:g:s:" opt; do
         # updatable options
         d) workdir=$OPTARG ;;
         a) jobname=$OPTARG ;;
-        r) GREMLIN_ITER=$OPTARG ;;
+        r) GREMLIN_ITER+=$OPTARG ;;
         m) MCR_DIR=$OPTARG ;;
         g) GREMLIN_DIR=$OPTARG ;;
         s) GREMLIN_SCRIPT_DIR=$OPTARG ;;
@@ -101,17 +101,22 @@ eval "$cmd" 2> ${log1}
 # read useful number from log1
 seq_len=$(tail -1 ${log1} |awk '{print $NF}')
 
-# run gremlin
-# takes tooooo loonnnnnnnnng for gremlin in matlab w/ single core, this should be replaced by GREMLIN_TF if possible
-log2=${workdir}/logs/${jobname}_gremlin_matlab.log
-cmd="$GREMLIN_DIR/run_gremlin.sh $MCR_DIR  ${msa_stem}.cut.msa ${msa_stem}.mtx MaxIter ${GREMLIN_ITER} verbose 1 apc 0"
-echo "$cmd"
-eval "$cmd" >${log2} 2>&1
 
-# generate matrix
-log3=${workdir}/logs/${jobname}_mtx2sco.log
-cmd="$GREMLIN_SCRIPT_DIR/mtx2sco.pl -mtx ${msa_stem}.mtx -cut ${msa_stem}.cut -div $(SeqLen ${fasta_1_path}) -seq_len ${seq_len} -apcd ${jobname}.apcd"
-echo "$cmd"
-eval "$cmd" > ${log3} 2>&1
+for i in $GREMLIN_ITER;
+do
+    # run gremlin
+    # takes tooooo loonnnnnnnnng for gremlin in matlab w/ single core, this should be replaced by GREMLIN_TF if possible
+    log2=${workdir}/logs/${jobname}_${i}_gremlin_matlab.log
+    cmd="$GREMLIN_DIR/run_gremlin.sh $MCR_DIR  ${msa_stem}.cut.msa ${msa_stem}_${i}.mtx MaxIter ${i} verbose 1 apc 0"
+    echo "$cmd"
+    eval "$cmd" >${log2} 2>&1
 
+    # generate matrix
+
+    log3=${workdir}/logs/${jobname}_${i}_mtx2sco.log
+    cmd="$GREMLIN_SCRIPT_DIR/mtx2sco.pl -mtx ${msa_stem}_${i}.mtx -cut ${msa_stem}.cut -div $(SeqLen ${fasta_1_path}) -seq_len ${seq_len} -apcd ${jobname}_${i}.apcd"
+    echo "$cmd"
+    eval "$cmd" > ${log3} 2>&1
+
+done
 popd
